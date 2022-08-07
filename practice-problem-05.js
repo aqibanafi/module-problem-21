d2689d3b9a21baf089047745d12219fca4007f5b
unknown <you@example.com>
2022-08-06 23:46:51 -0700

    done

--- practice-problem-05.js
+++ practice-problem-05.js
@@ -1,13 +1,21 @@
 let arr = [20, 18, 37, 11, 9, 54, 36];
 let result;
 let maxNumber = arr[0];
+let secondMaxNumber = arr[1];
 function find_max_number (arr) {
     for (i = 1; i < 8; i++){
         if (maxNumber < arr[i]){
             maxNumber = arr[i];
         }
     }
-    return maxNumber;
+    let maxNumberPosition = arr.indexOf(maxNumber)
+    console.log(maxNumberPosition);
+    for (i = 1; i < 8; i++) {
+        if (arr[i] != arr[maxNumberPosition] && secondMaxNumber < arr[i]) {
+            secondMaxNumber = arr[i];
+        }
+    }
+    return secondMaxNumber;
 }
 
 let output = find_max_number (arr);
